ebdbc412e28d2bedeb3b8ea3c58e534dde15ff46
yashh1 <34425222+yashh1@users.noreply.github.com>
2018-02-10 14:37:12 +0530

    Add files via upload

---
+++ silhoutte.py
@@ -0,0 +1,42 @@
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import random
+
+from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+
+from sklearn.metrics import silhouette_score, silhouette_samples
+
+dataset=load_files("./preprocess/data", description=None, categories="alt,comp", load_content=True, shuffle=True, encoding=None, decode_error='strict', random_state=0)
+
+categories= list(dataset.target_names)
+
+num_categories=len(dataset.target_names)
+
+orig_labels = dataset.target_names      # target_names => individual 'y' values for each of the documents : (In this case: 0(=>alt) through 6(=>soc))
+
+vectorizer = TfidfVectorizer()
+vectors = vectorizer.fit_transform(dataset.data)
+
+vectors=vectors.todense()
+print("%d features " % vectors.shape[1])
+
+"""
+clusters= kmeans_iter(vectors.tolist(),labels,20)
+cluster_labels=make_assumption(clusters, labels,num_categories)
+"""
+clusterer = KMeans(n_clusters=len(dataset.target_names))
+cluster_labels = clusterer.fit_predict(vectors)
+
+silhouette_avg = silhouette_score(vectors, cluster_labels)
+
+# Compute the silhouette scores for each sample
+sample_silhouette_values = silhouette_samples(vectors, cluster_labels)
+
+print ("The silhouette_avg is: ", 1-silhouette_avg)
+
+print ("Silhoutte sample values: ")
+for i in range(len(dataset.target_names)):
+    print (i, " : ", sample_silhouette_values[cluster_labels==i])
+
---
+++ test.py
@@ -0,0 +1,42 @@
+from sklearn.datasets import load_files
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+import random
+from seededkmeans import *
+from elm import *
+from clusterassignment import *
+from validitychecks import *
+from morechecks import *
+
+dataset=load_files("./preprocess/data", description=None, categories="alt", load_content=True, shuffle=False, encoding=None, decode_error='strict', random_state=0)
+
+categories= list(dataset.target_names)
+
+print("%d documents" % len(dataset.data))
+print("%d categories" % len(dataset.target_names))
+print(list(dataset.target_names))
+
+num_categories=len(dataset.target_names)
+orig_labels = dataset.target
+orig_labels = map(lambda x: x + 1, orig_labels)
+
+labels=list(orig_labels)
+toRemove = random.sample(range(0,len(dataset.data)), int(0.9*len(dataset.data)));
+for x in toRemove:
+    labels[x]=0
+
+vectorizer = TfidfVectorizer()
+vectors = vectorizer.fit_transform(dataset.data)
+
+
+vectors=vectors.todense()
+print("%d features " % vectors.shape[1])
+
+elmvectors = elmMap(vectors,15400,'sigmoid')
+print ("Number of Hidden Neurons: 15400")
+numberOfIterations=5
+clusters,centroids= kmeans_iter(elmvectors.tolist(),labels,numberOfIterations)
+cluster_labels=assign_clusters(clusters, labels)
+moremeasures(clusters,elmvectors,centroids)
+print (elmvectors[0])
+
